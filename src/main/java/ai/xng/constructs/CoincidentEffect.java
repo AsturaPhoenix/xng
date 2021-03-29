44270c0b00a5bdc9b70d693c3f61808643ec0113
Ross Wang <imagipioneer@gmail.com>
2021-03-29 14:13:41 -0700

    Fix lapsed listener in CoincidentEffect

--- src/main/java/ai/xng/constructs/CoincidentEffect.java
+++ src/main/java/ai/xng/constructs/CoincidentEffect.java
@@ -3,12 +3,15 @@ package ai.xng.constructs;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.Serializable;
+import java.lang.ref.WeakReference;
 
 import ai.xng.ActionCluster;
 import ai.xng.IntegrationProfile;
 import ai.xng.Posterior;
 import ai.xng.PosteriorCluster;
 import ai.xng.Scheduler;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 import lombok.val;
 
 public abstract class CoincidentEffect<T extends Posterior> implements Serializable {
@@ -48,13 +51,49 @@ public abstract class CoincidentEffect<T extends Posterior> implements Serializa
     }
   }
 
-  private void subscribe() {
-    cluster.rxActivations().subscribe(node -> {
-      val integrator = CoincidentEffect.this.node.getIntegrator();
-      if (!integrator.isPending() && integrator.isActive()) {
-        apply(node);
+  /**
+   * Avoids a lapsed-listener leak using a weak reference to the outer class.
+   * There is no test covering this leak since it manifests only in transient
+   * memory, the usage of which we do not have a great way to guage.
+   * <p>
+   * TODO: We might be able to do test this with Instrumentation.
+   */
+  private static class WeakSubscription<T extends Posterior> implements Observer<T> {
+    final WeakReference<CoincidentEffect<T>> weakParent;
+    Disposable subscription;
+
+    WeakSubscription(final CoincidentEffect<T> parent) {
+      weakParent = new WeakReference<>(parent);
+    }
+
+    @Override
+    public void onSubscribe(final Disposable d) {
+      subscription = d;
+    }
+
+    public void onNext(final T node) {
+      val parent = weakParent.get();
+      if (parent == null) {
+        subscription.dispose();
+      } else {
+        val integrator = parent.node.getIntegrator();
+        if (!integrator.isPending() && integrator.isActive()) {
+          parent.apply(node);
+        }
       }
-    });
+    }
+
+    @Override
+    public void onComplete() {
+    }
+
+    @Override
+    public void onError(Throwable e) {
+    }
+  }
+
+  private void subscribe() {
+    cluster.rxActivations().subscribe(new WeakSubscription<T>(this));
   }
 
   private void readObject(final ObjectInputStream o) throws ClassNotFoundException, IOException {
